dc12b0b5e7a4e88768522949cd486a51f2b52d20
Minabombino <kkalininsky@gmail.com>
2026-03-05 15:29:13 +0300

    сделаны запросы /{id}; /all

--- src/main/java/com/caselab/task/model/Book.java
+++ src/main/java/com/caselab/task/model/Book.java
@@ -1,14 +1,36 @@
 package com.caselab.task.model;
 
-public class Book {
-    private Long id;
-    private String title;
-    private String author;
-    private Integer year;
 
-    public Book(int i, String мастерИМаргарита, String булгаков, int i1) {
+import lombok.Setter;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Book {
+    public int id;
+    public String title;
+    public String author;
+    public Integer year;
+
+    public Book(int id, String title, String author, int year) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.year = year;
+     }
+     
+
+    public Book() {
+
     }
-    public static Book FindById(Long id) {
+
+    public static Book FindById(int id, List<Book> books) {
+        // здесь мы делаем поиск по массиву this.books
+        for (int i = 0; i < books.toArray().length; i++) {
+            if (books.get(i).id == id) {
+                return books.get(i);
+            }
+        }
         return null;
     }
 }
--- src/main/java/com/caselab/task/modules/book/controller/BookController.java
+++ src/main/java/com/caselab/task/modules/book/controller/BookController.java
@@ -4,22 +4,33 @@ import com.caselab.task.model.Book;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
+@RestController
 public class BookController {
-    List<Book> books = Arrays.asList(
+    public List<Book> books = Arrays.asList(
             new Book(1, "Война и мир", "Толстой", 1869),
             new Book(2, "Преступление и наказание", "Достоевский", 1866),
             new Book(3, "Мастер и Маргарита", "Булгаков", 1967)
     );
+
+
     @GetMapping("/{id}")
-    public ResponseEntity<Book> getBookById(@PathVariable Long id){
-       Book book= Book.FindById(id);
-       if (book != null){
+    public ResponseEntity<Book> getBookById(@PathVariable int id){
+
+        // Вообще это костыль, но у нас данные в памяти
+        Book book = Book.FindById(id, books);
+
+        if (book != null){
             return ResponseEntity.ok(book);
-       }
-       return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+    @GetMapping("/all")
+    public ResponseEntity<List<Book>> getAllBook(){
+        return ResponseEntity.ok(books);
     }
 }
