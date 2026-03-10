ec5c839dca28d2c9ab4caca4e4c2436818f6a0ab
Minabombino <kkalininsky@gmail.com>
2026-03-10 21:24:17 +0300

    @Requestmapping+починил права директорий

--- project/src/main/java/com/caselab/task/modules/book/controller/BookController.java
+++ project/src/main/java/com/caselab/task/modules/book/controller/BookController.java
@@ -4,12 +4,14 @@ import com.caselab.task.model.Book;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/book")
 public class BookController {
     public List<Book> books = Arrays.asList(
             new Book(1, "Война и мир", "Толстой", 1869),
